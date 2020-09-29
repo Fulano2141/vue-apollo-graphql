95cd00adc1ff93e7ad3be4fcbf4bf9578e2a0632
Fulano2141 <macuto2142dt@gmail.com>
2020-09-29 18:37:55 -0400

    graphql and vue local

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { createProvider } from './vue-apollo'
+import VueApollo from 'vue-apollo'
+// import ApolloClient from 'apollo-boost'
 
 Vue.config.productionTip = false
-
+// const apolloClient = new ApolloClient({
+//   // You should use an absolute URL here
+//   uri: 'https://api.graphcms.com/simple/v1/awesomeTalksClone'
+// })
+Vue.use(VueApollo)
 new Vue({
-  render: h => h(App),
+  apolloProvider: createProvider(),
+  render: h => h(App)
 }).$mount('#app')
